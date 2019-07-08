9ea856e086e7acca81e307c72a2ab3632c68c3b8
Benjamin <Ben.Lung@gmx.de>
2019-07-08 12:15:43 +0200

    small updates to testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,10 @@ This module is always executed automatically when pytest is started
 # set Pythonpath to include modue code
 import sys
 import os
-new_paths = [os.path.abspath('../code'), os.path.abspath('../tests'),]
-for new_path in new_paths:
-    if new_path not in sys.path:
-        sys.path.append(new_path)
+new_path = os.path.abspath('../code')
+if new_path not in sys.path:
+    sys.path.append(new_path)
 
-# import fixtures
-from fixtures import * # one of the very few cases where import * is OK
+# import fixtures - must be after setting Pythonpath !
+from fixtures import * # one of the few cases where import * is OK
+# import more fixture files here, if required
