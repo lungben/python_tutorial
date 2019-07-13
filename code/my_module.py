a28a41776a0220deb8e9030d6bec482c957c3f3f
Benjamin <Ben.Lung@gmx.de>
2019-07-13 20:58:23 +0200

    inspection added

--- code/my_module.py
+++ code/my_module.py
@@ -1,4 +1,4 @@
-# example file for module imports
+"""example file for module imports"""
 
 __version__ = '0.0.1'
 
@@ -7,12 +7,20 @@ my_list = [1, 2, 3, 4]
 my_tuple = ('a', 'b', 'c')
 
 def double_me(x):
+    """doubles input value"""
     return 2*x
 
+def power_me(x, i: int, times_two=False):
+    """useless example function"""
+    mult = 2 if times_two else 1
+    return mult*x**i
+
 class Multiplier:
+    """class that creates multiplier objects with a given factor"""
     def __init__(self, i):
         self.i = i
     def get_result(self, x):
+        """returns factor specified with initialization multiplied by input value"""
         return self.i*x
 
 mult2 = Multiplier(2)
